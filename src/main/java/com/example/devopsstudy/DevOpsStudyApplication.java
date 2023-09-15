2a7477b47c1f1440098f82140fdfca7162717671
user <b57062562@gmail.com>
2023-09-15 13:27:28 +0900

    Initial commit

---
+++ src/main/java/com/example/devopsstudy/DevOpsStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.devopsstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevOpsStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/devopsstudy/DevOpsStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devopsstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
