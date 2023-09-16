236333dad04d74a5392afd3d13c1cc5f8a1a2932
user <b57062562@gmail.com>
2023-09-16 15:44:09 +0900

    ♻️ :: yml 수정

--- src/main/java/com/example/devopsstudy/SecurityConfig.java
+++ src/main/java/com/example/devopsstudy/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.example.devopsstudy;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,7 +9,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
-@RequiredArgsConstructor
 public class SecurityConfig {
 
     @Bean
